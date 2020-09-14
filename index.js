89a3d9d380a0995b91a8998fdd8df9cb6b602e8e
Zachery Sheley <zacherysheley@protonmail.com>
2020-09-14 13:30:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function myFunction(imgs) {
   expandImg.parentElement.style.display = "block";
   //test favicon
   var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
+    link.type = 'image/png';
     link.rel = 'shortcut icon';
-    link.href = 'http://www.stackoverflow.com/favicon.ico';
+    link.href = 'favicon.png';
     document.getElementsByTagName('head')[0].appendChild(link);
 }
